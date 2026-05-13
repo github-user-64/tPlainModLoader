451a482af72ae952ee3c1dbd2789020163a77af2
Love <1870407453@qq.com>
2026-05-13 21:03:27 +0800

    内存问题
    解决直接启动闪退问题

--- tPlainModLoader/tContentPatch/Utils/Log.cs
+++ tPlainModLoader/tContentPatch/Utils/Log.cs
@@ -1,42 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace tContentPatch.Utils
 {
-    /// <summary/>
     public static class Log
     {
-        /// <summary/>
         public static string path { get; private set; } = null;
         private static List<string> logs = new List<string>();
+        private static DateTime lastSaveTime = DateTime.Now;
+        private const int MAX_LOG_COUNT = 1000;
+        private const int SAVE_INTERVAL_MINUTES = 5;
 
-        /// <summary/>
         public static void Add(string s)
         {
             DateTime time = DateTime.Now;
 
             string ss = $"[{time.Hour}:{time.Minute}:{time.Millisecond}]:{s}\n";
             logs.Add(ss);
+
+            bool shouldSave = false;
+
+            if (logs.Count >= MAX_LOG_COUNT)
+            {
+                shouldSave = true;
+            }
+            else if ((DateTime.Now - lastSaveTime).TotalMinutes >= SAVE_INTERVAL_MINUTES)
+            {
+                shouldSave = true;
+            }
+
+            if (shouldSave)
+            {
+                SaveTry();
+                lastSaveTime = DateTime.Now;
+            }
         }
 
-        /// <summary/>
         public static void SaveTry()
         {
             try
             {
                 string file = path;
-                string s = "";
-                for (int i = 0; i < logs.Count; ++i) s += logs[i];
 
                 if (Directory.Exists(Path.GetDirectoryName(file)) == false) file = Path.GetFileName(file);
-                File.WriteAllText(file, s, Encoding.UTF8);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < logs.Count; ++i) sb.Append(logs[i]);
+
+                File.AppendAllText(file, sb.ToString(), Encoding.UTF8);
+                logs.Clear();
             }
             catch { }
         }
 
-        /// <summary/>
         public static void SetPath(string filePath)
         {
             if (filePath == null) return;
